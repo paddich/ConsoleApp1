3bdfdd45f9b5139e1cf27842847aeee34f9ec7a1
paddich <skachkovaa.work@gmail.com>
2022-10-24 14:04:30 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            static string GetLastHalf(string text)
+            {
+                int len = text.Length / 2;
+                string half = text.Remove(0, len);
+                string result = half.Replace(" ", "");
+                return result;
+            }
+
+            Console.WriteLine(GetLastHalf("I love CSharp!"));
+            Console.WriteLine(GetLastHalf("1234567890"));
+            Console.WriteLine(GetLastHalf("до ре ми фа соль ля си"));
+        }
+    }
+}
